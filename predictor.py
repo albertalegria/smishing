a61d7bce8baa6c44d9016083dfc117cb52c5025c
albertalegria <albertalegria@gmail.com>
2023-07-18 19:34:14 +0200

    Translation added if message not en

--- predictor.py
+++ predictor.py
@@ -1,9 +1,20 @@
 from model import RandomForestSpamClassifier
+from langdetect import detect
+import translators as ts
 
 class Predictor:
     def __init__(self, model_path):
         self.classifier = RandomForestSpamClassifier()
         self.classifier.load_model(model_path)
 
+    def translate_to_english(self, text):
+        if detect(text) != 'en':
+            translated_text = ts.translate_text(text)  # translate to English
+            return translated_text
+        else:
+            return text
+
     def predict(self, message):
-        return self.classifier.predict(message)
+        translated_message = self.translate_to_english(message)
+        return self.classifier.predict(translated_message)
+
